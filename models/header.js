348740823d910620505ff65c41d55640acd5f6d9
Dummi <raydecrom@hotmail.com>
2023-04-26 18:13:20 +0200

    background check!

--- models/header.js
+++ models/header.js
@@ -1,7 +1,33 @@
 const header = document.getElementById('header');
 
-header.innerHTML += `
-<a class="header-link" href="#"><button class="header-button">Home</button></a>
-<a class="header-link" href="#"><button class="header-button">Socials</button></a>
-<a class="header-link" href="#"><button class="header-button"></button></a>
+if (header) {
+	header.innerHTML += `
+    <a class="header-link" href="/">Home</a>
+    <a class="header-link" href="/pages/news">News</a>
+    <div id="menu" class="header-link">
+        Socials
+    
+        <div id="options" class="hidden">
+        <li><a href="https://www.facebook.com/Colourtheoneandonly" target="_blank">Facebook</a></li>
+        <li><a href="https://www.instagram.com/colourtheoneandonly/" target="_blank">Instagram</a></li>
+        <li><a href="https://www.youtube.com/channel/UCB7Ut11YYhtLi3_RidLaMlg" target="_blank">YouTube</a></li>
+        <li><a href="https://open.spotify.com/artist/199B2dCRTpPAPqdoD5mKrk" target="_blank">Spotify</a></li>
+        <li><a href="https://colourtheoneandonly.bandcamp.com/album/colour" target="_blank">Bandcamp</a></li>
+        </div>
+    </div>
+    <a class="header-link" href="/pages/colour#contact">Contact</a>
 `;
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	const menu = document.getElementById('menu');
+	const options = document.getElementById('options');
+
+	if (menu) {
+		menu.addEventListener('click', (event) => {
+			const state = options.classList.contains('hidden');
+			if (state) options.classList.remove('hidden');
+			else options.classList.add('hidden');
+		});
+	}
+});
