e1d0371c8740e3dc35af402ea0fa16c2bcc1b0d0
TheDummi <raydegroot14@gmail.com>
2023-08-08 17:20:20 +0200

    concerts

---
+++ models/concerts.js
@@ -0,0 +1,7 @@
+/** @format */
+
+const concerts = document.getElementById('Concerts');
+
+if (document.URL.includes('colour')) {
+	const links = ['aaa', 'aaa', 'aaa', 'aaa', 'aaa', 'bbb', 'bbb', 'bbb', 'bbb'];
+}
